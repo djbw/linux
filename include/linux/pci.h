ad3b92443603de6c273b30d2fe1a190de91fff63
Dan Williams <dan.j.williams@intel.com>
2024-11-13 14:23:23 -0800

    PCI: Add PCIe Device 3 Extended Capability enumeration
    PCIe 6.2 Section 7.7.9 Device 3 Extended Capability Structure,
    enumerates new link capabilities and status added for Gen 6 devices. One
    of the link details enumerated in that register block is the "Segment
    Captured" status in the Device Status 3 register. That status is
    relevant for enabling IDE (Integrity & Data Encryption) whereby
    Selective IDE streams can be limited to a given requester id range
    within a given segment.
    
    If a device has captured its Segment value then it knows that PCIe Flit
    Mode is enabled via all links in the path that a configuration write
    traversed. IDE establishment requires that "Segment Base" in
    IDE RID Association Register 2 (PCIe 6.2 Section 7.9.26.5.4.2) be
    programmed if the RID association mechanism is in effect.
    
    When / if IDE + Flit Mode capable devices arrive, the PCI core needs to
    setup the segment base when using the RID association facility, but no
    known deployments today depend on this.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -434,6 +434,7 @@ struct pci_dev {
 	unsigned int	ats_enabled:1;		/* Address Translation Svc */
 	unsigned int	pasid_enabled:1;	/* Process Address Space ID */
 	unsigned int	pri_enabled:1;		/* Page Request Interface */
+	unsigned int	fm_enabled:1;		/* Flit Mode (segment captured) */
 	unsigned int	is_managed:1;		/* Managed via devres */
 	unsigned int	is_msi_managed:1;	/* MSI release via devres installed */
 	unsigned int	needs_freset:1;		/* Requires fundamental reset */
